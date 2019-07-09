56b725fa02e2e93143af2ae621128b751a9a5fef
ritu kashyap <ritukashyap478@gmail.com>
2019-07-09 21:41:14 +0530

    added common

---
+++ resources/lang/it/common.php
@@ -0,0 +1,378 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+     */
+
+    'contact_page_title' => 'Contattaci',
+    'contact_page_information' => 'INFORMAZIONI SUI CONTATTI',
+    'contact_page_address' => 'INDIRIZZO',
+    'contact_page_phone' => 'TELEFONO',
+    'contact_page_email' => 'E-MAIL',
+    'contact_page_getintouch' => 'RICEVI IN TOCCO',
+    'contact_page_firstname' => 'Nome di battesimo',
+    'contact_page_lastname' => 'Cognome',
+    'contact_page_youremail' => 'La tua email',
+    'contact_page_phoneno' => 'Numero di telefono',
+    'contact_page_message' => 'Messaggio',
+    'contact_page_submit' => 'Sottoscrivi',
+
+    // Blog Page
+    'blog_page_title' => 'Blog',
+    'blog_page_recent_posts' => 'messaggi recenti',
+    'blog_page_by' => 'Di',
+    'blog_page_read_more' => 'Leggi di più..',
+    'blog_page_archive' => 'Archivio',
+    'blog_page_search' => 'Ricerca',
+    'blog_page_search_not_found' => 'Nessun blog trovato !!',
+
+    // Login Page
+    'login_page_title' => 'Accesso',
+    'login_page_subtitle' => 'Accedi al tuo account',
+    'login_page_emailaddress' => 'Indirizzo email',
+    'login_page_password' => 'Parola d\'ordine',
+    'login_page_signin' => 'Registrati',
+    'login_page_forgotpassword' => 'Ho dimenticato la mia password',
+    'login_page_registeropt' => 'Nessun account? Registrati qui.',
+
+    // Signup Page
+    'reg_title' => 'Iscriviti',
+    'reg_create_an_acc' => "CREA UN ACCOUNT",
+    'reg_customer_tab' => 'Cliente',
+    'reg_customer_countrycode' => 'Prefisso internazionale',
+    'reg_servicer_tab' => 'Fornitore di servizi',
+
+    // Account Page
+    'ac_page_hello' => 'Ciao',
+    'ac_page_pi' => 'Informazione personale',
+    'ac_page_mobile' => 'Numero di cellulare',
+    'ac_page_changepassword' => 'Cambia la password',
+    'ac_page_odrers' => 'i miei ordini',
+    'ac_page_dashboard' => 'Cruscotto',
+    'ac_page_notifications' => 'notifiche',
+    'ac_page_inbox' => 'Posta in arrivo',
+    'ac_page_requests' => 'Richiesta',
+    'ac_page_payments' => 'pagamenti',
+    'ac_page_addresses' => 'Indirizzo',
+    'ac_page_logout' => 'Disconnettersi',
+    'ac_page_myorders' => 'i miei ordini',
+    'ac_page_no_orders_yet' => 'Nessun ordine ancora.',
+    'ac_page_manage_add' => 'Gestisci indirizzi',
+    'ac_page_addnew_address' => 'Aggiungi un nuovo indirizzo',
+    'ac_page_no_address' => 'Nessun indirizzo creato ancora.',
+    'ac_page_shop_now' => 'Acquistare ora!.',
+
+    // Forgot Password
+    'resetpass_page_title' => 'Ha dimenticato la password',
+    'resetpass_page_subtitle' => 'HAI DIMENTICATO LA PASSWORD?',
+    'resetpass_page_sub_desc' => 'Inserisci l\'email, che hai usato per registrarti. Invieremo quindi il link per la reimpostazione della password.',
+    'resetpass_page_email' => 'Indirizzo email',
+    'resetpass_page_button' => 'recupera password',
+    'resetpass_page_backto_login' => 'Torna al login',
+
+    // Reset Password
+    'updatepass_page_title' => 'Resetta la password',
+    'updatepass_page_subtitle' => 'Reimposta la tua password',
+
+    'updatepass_page_email' => 'Indirizzo email',
+    'updatepass_page_password' => 'Parola d\'ordine',
+    'updatepass_page_confirm_password' => 'conferma password',
+    'updatepass_page_button' => 'Resetta la password',
+
+    // Home Page
+    'home_page_about' => 'Di <strong>Noi</strong>',
+    'home_page_ourapp' => '	AdBlue <strong>App </strong>',
+    'home_page_blogs' => 'Nostra <strong>Blog</strong>',
+    'home_page_prodcuts' => 'Nostra <strong>Prodotti</strong>',
+    'home_page_readmore' => 'Leggi di più',
+    'home_page_download_app' => 'Scarica l\'app',
+    'home_page_view_all' => 'Guarda tutto',
+
+    // About Us page
+    'about_page_title' => 'Riguardo a noi',
+    'about_page_ourteam' => 'Nostra <strong>Squadra</strong>',
+
+    // Our Apps
+    'apps_page_title' => 'Apps',
+
+    // Stations
+    'stations_page_title' => 'Le nostre pompe',
+    'stations_station_name_search' => 'Cerca per nome della pompa',
+    'stations_address_search' => 'Cerca per indirizzo',
+    'stations_miles_all' => 'Tutte',
+    'stations_miles_5' => '5 Miglia',
+    'stations_miles_10' => '10 Miglia',
+    'stations_miles_20' => '20 Miglia',
+    'stations_miles_50' => '50 Miglia',
+    'stations_miles_100' => '100 Miglia',
+    'stations_byn_search' => 'Ricerca',
+
+    // Station detail page
+    'station_location_ofstore' => 'Posizione dello Store:',
+    'station_related_products' => 'prodotti correlati',
+    'station_view_btn' => 'Visualizza il prodotto',
+    'station_related_products_not_found' => 'Nessun prodotto trovato.',
+
+    // Routes Section
+    'routes_page_title' => 'Itinerari',
+    'routes_filter_button_text' => 'Filtro',
+    'routes_button_apply' => 'Applicare',
+    'routes_label_from' => 'A partire dal',
+    'routes_label_to' => 'A',
+    'routes_first_location' => 'Posizione non aggiunta.',
+    'routes_last_location' => 'Posizione non aggiunta.',
+    'routes_label_at' => 'a',
+    'routes_label_date' => 'Data',
+    'routes_label_price' => 'Prezzo',
+
+    'routes_search_location' => 'Posizione',
+    'routes_search_date' => 'Data adatta',
+
+    'routes_create_request_btn' => 'Visualizza le informazioni complete sul percorso',
+    'routes_no_route_found' => 'Nessun percorso trovato.',
+
+    // Route info page
+
+    'route_distance_to_cover' => 'Distanza dalla copertura',
+    'route_volume_contained' => 'Volume contenuto',
+    'route_volume_remaining' => 'Olio rimanente in cisterna',
+    'route_driver' => 'autista',
+    'route_within_text' => 'Entro',
+    'route_notify_user' => 'Avvisare gli utenti',
+    'route_create_req_btn' => 'Crea richiesta',
+
+    // Create Request Pop up
+    'route_req_popup_title' => 'Crea richiesta',
+    'route_req_popup_input_add' => 'Inserisci il tuo indirizzo',
+    'route_req_popup_input_location' => 'Scegli la posizione del percorso ',
+    'route_req_popup_input_qty' => 'Quantità ',
+        'route_req_popup_input_btn' => 'Invia richiesta ',
+
+    'route_cancel_req_btn' => 'Richiesta cancellata',
+
+    // Not logged in user pop up on route info page
+    'route_notloggedin' => 'Per creare la richiesta, è necessario prima effettuare il login all\'account. ',
+    'route_loginbtn' => 'Clicca qui per accedere',
+
+    // Cancel Popup
+    'route_cancel_confirm' => 'Annullare la richiesta?',
+    'route_cancel_confirm_yes' => 'sì',
+    'route_cancel_confirm_no' => 'No',
+
+    // Cancel Request
+    'route_cancelled_true' => 'Richiesta cancellata con successo.',
+    'route_cancelled_msg' => 'Richiesta annullata',
+    'route_fulfilled_already' => 'Richiesta soddisfatta con successo.',
+    'route_cancelled_false' => 'Opps! errore si è verificato, per favore riprova.',
+
+    // Messages on crete request pop up
+    'route_req_route_notfound' => 'Percorso non trovato',
+    'route_req_route_insufficient_msg' => "Siamo spiacenti, la tua richiesta non può essere elaborata a causa di insufficiente olio in una nave cisterna.",
+    'route_req_route_success_msg' => "Richiesta inviata con successo.",
+    'route_req_route_error_msg' => "Errore durante la richiesta di invio.",
+
+    // Account Requests page
+    'requests_page_pending_tab' => 'in attesa di',
+    'requests_page_delivered_tab' => 'consegnato',
+    'requests_page_driver_name' => 'Nome del driver',
+    'requests_page_address' => 'Indirizzo',
+    'requests_page_quantity' => 'Quantità',
+    'requests_page_confirmed_status' => 'Confermata',
+    'requests_page_cancelled_status' => 'Annullata',
+
+    // Shop Page
+    'shop_page_title' => 'Negozio di prodotti',
+    'category_page_title' => 'Categoria: ',
+
+    'search_page_title' => 'RISULTATI DI RICERCA',
+    'search_page_noproducts' => 'Niente trovato con le tue parole chiave di ricerca.',
+
+    // Single Product page
+    'product_page_title' => 'Prodotti',
+    'product_breadcrumb_hometitle' => 'Casa',
+    'product_breadcrumb_product_title' => 'Prodotto',
+
+    // Product Listing Row
+
+    'listproduct_sale' => 'saldi',
+    'listproduct_addtocart' => 'Aggiungi al carrello ',
+    'listproduct_noproduct' => 'Nessun prodotto ancora',
+    'listproduct_sortby' => 'Ordina per',
+
+    'listproduct_sortbyop1' => ' Nome  (a-z)',
+    'listproduct_sortbyop2' => ' Nome  (z-a)',
+    'listproduct_sortbyop3' => 'Prezzo (basso - alto)',
+    'listproduct_sortbyop4' => 'Prezzo (alto - basso)',
+
+    'listproduct_single_product' => 'Prodotto',
+    'listproduct_multiple_products' => 'Prodotti',
+
+    // Product sidebar
+    'product_sidebar_filter' => 'Filtro',
+    'product_sidebar_byprice' => 'Prezzo del prodotto',
+    'product_sidebar_bycategory' => 'Per categorie',
+    'product_sidebar_bybrands' => 'Per marche di prodotti',
+    'product_sidebar_byservice' => 'Tipo di servizio offerto',
+    'product_sidebar_byservice_home' => 'Consegna a domicilio disponibile',
+    'product_sidebar_byservice_pick' => 'Raccogliere',
+    'product_rating_label' => 'Valutazione e recensioni',
+    'product_norating' => 'Nessun voto ancora.',
+    'product_review_left' => 'Tutte',
+    'product_review_right' => 'Recensioni.',
+
+    // Footer
+    'footer_newsletter' => 'ISCRIVITI ALLA NOSTRA NEWSLETTER',
+    'footer_connect_with_us' => 'METTITI IN COMUNICAZIONE CON NOI',
+    'footer_quicklink' => 'LINK VELOCI',
+    'footer_subscribe_btn' => 'sottoscrivi',
+    'footer_textbox_placeholder' => 'Il tuo miglior indirizzo email',
+
+    'footer_home' => 'Casa',
+    'footer_about_us' => 'Riguardo a noi',
+    'footer_stations' => 'Pompe',
+    'footer_products' => 'Prodotti',
+    'footer_blog' => 'Blog',
+    'footer_contact_us' => 'Contattaci',
+    'footer_your_acc' => 'Il tuo account',
+
+    'footer_contact_label' => 'CONTATTACI',
+
+    // Account Address
+    'ac_page_addnew_alias' => "i.e. Ufficio a casa",
+    'ac_page_addnew_phoneno' => "Numero di telefono",
+    'ac_page_addnew_address1' => "Indirizzo (area e via)",
+    'ac_page_addnew_address2' => "Indirizzo 2",
+    'ac_page_addnew_country' => "Nazione",
+    'ac_page_addnew_zipcode' => "Cap",
+    'ac_page_addnew_saveadd_btn' => "Salva indirizzo",
+    'ac_page_add_edit' => "Modifica indirizzo",
+
+    'ac_dashboard_title' => 'Informazione personale',
+    'ac_dashboard_mobileno' => 'Cellulare n',
+    'ac_dashboard_updateInfo' => 'Aggiornare le informazioni',
+    'ac_dashboard_changepassword' => 'Cambia la password',
+
+    'ac_dashboard_popup_top_heading' => 'La tua nuova password deve:',
+    'ac_dashboard_popup_instruction_1' => 'Avere almeno 6 caratteri di lunghezza',
+    'ac_dashboard_popup_instruction_2' => 'Non essere uguale alla tua password attuale',
+    'ac_dashboard_popup_right_heading' => 'Cambia il tuo passoword',
+
+    'ac_dashboard_popup_current_password' => 'Password attuale',
+    'ac_dashboard_popup_new_password' => 'nuova password',
+    'ac_dashboard_popup_comfirm_new_password' => 'Conferma la nuova password',
+    'ac_dashboard_popup_btn' => 'Cambia la password',
+    'ac_dashboard_editpic' => 'Modifica foto',
+
+    'ac_page_update_title' => "Indirizzo di aggiornamento",
+    'ac_page_update_btn' => "Aggiornare",
+    'ac_page_back_btn' => "Indietro",
+
+    // Product Page
+    'p_brand' => "Marca",
+    'p_seller' => "Venditrice",
+    'p_product_sizes' => "Dimensioni del prodotto",
+    'p_select_size' => "Seleziona la dimensione",
+    'p_qty' => "Quantità",
+    'p_type_of_service' => "Tipo di servizio offerto",
+    'p_delivery_charges' => "Spese di spedizione",
+    'p_add_to_cart' => "Aggiungi al carrello",
+    'p_location_of_store' => "Posizione dello Store ",
+    'p_rating_reviews' => "Valutazioni e recensioni ",
+
+    'p_cart_popup_title' => "Sostituisci l'articolo del carrello",
+    'p_cart_popup_content' => "Il tuo carrello contiene prodotti dal fornitore di servizi ",
+    'p_cart_popup_content2' => " Vuoi scartare la selezione e aggiungere prodotti da ",
+
+    'ac_payments' => "Gestisci le carte salvate",
+    'ac_payments_addnew' => "Aggiungi una nuova carta",
+    'ac_payments_save_button' => "Salva questa carta",
+    'ac_payments_cancel_button' => "Annulla",
+    'ac_payments_card_number' => "Inserisci il numero della carta",
+    'ac_payments_expiry' => "Scadenza",
+    'ac_payments_nameoncard' => "Nome sulla carta",
+    'ac_payments_cvc' => "CVC",
+    'ac_card_del_confirm' => "Sei sicuro?",
+
+    // Cart
+    'cart_heading' => "Carrello della spesa",
+    'cart_update_btn' => "Aggiornare",
+    'cart_product_seller' => "Venditrice",
+    'cart_continue_shopping' => "Continua a fare acquisti",
+    'cart_place_order' => "Invia ordine",
+    'cart_price_details' => "Dettagli del prezzo",
+    'cart_tax' => "Imposta",
+    'cart_total_amt_pay' => "Importo totale pagabile",
+
+    // Checkout
+    'ck_breadcrumb_home' => "Casa",
+    'ck_breadcrumb_cart' => "Carrello",
+    'ck_breadcrumb_checkout' => "Check-out",
+    'ck_welcome' => "Benvenuta!!",
+    'ck_delivery_add' => "Indirizzo di consegna",
+    'ck_billing_add' => "Indirizzo Di Fatturazione",
+    'ck_same_as_billing' => " Come da fattura",
+    'ck_manage_address' => " modificare",
+    'ck_payments' => " Scegli la tua carta per il pagamento",
+    'ck_manage_payments' => " Gestisci le carte",
+    'ck_added_cardtype' => " Tipo di carta: ",
+    'ck_added_expiry' => " Scadenza: ",
+    'ck_added_lastdigits' => "Ultime cifre: ",
+    'ck_added_nameoncard' => "Nome sulla carta: ",
+    'ck_product_seller' => "Venditrice: ",
+
+    'ck_continue' => " Continua",
+    'ck_samedelivery_label' => " Indirizzo di consegna",
+    'ck_order_summery' => " Riepilogo dell'ordine",
+    'ck_payment_opt' => " Opzioni di pagamento",
+    'ck_no_payment_method_set' => " Nessuna carta aggiunta per il pagamento, per favore aggiungi prima la carta. ",
+    'ck_no_address_found' => " Nessun indirizzo trovato. Devi prima creare un indirizzo qui.",
+    'ck_right_price_detail' => " Dettagli del prezzo",
+    'ck_right_price_subtotal' => " Totale parziale",
+    'ck_right_delivery_charges' => " Spese di spedizione",
+    'ck_right_amount_payable' => " Importo da pagare",
+    'ck_shopping_cart' => " Carrello della spesa",
+    'ck_shopping_cart_empty' => " il tuo carrello è vuoto.",
+    'ck_shopping_shopnow' => " Acquistare ora!",
+
+    // Checkout succcess
+    'ck_order_success' => " Il tuo ordine è stato inoltrato!",
+    'ck_order_success_line_first' => "Il tuo ordine è stato elaborato con successo!",
+    'ck_order_success_line_second' => "Si prega di indirizzare eventuali domande che avete al proprietario del negozio. ",
+    'ck_order_success_line_third' => "Grazie per lo shopping con noi online! ",
+    'ck_order_success_continue' => "Continua!",
+
+    // Checkout Cancel
+    'ck_order_cancel_1' => "Hai annullato il tuo ordine. Forse lo vuoi",
+    'ck_order_cancel_2' => "checkout altri articoli?",
+
+    // OTP Page
+
+    'otp_title' => "Conferma OTP",
+    'otp_desc' => "Inserire il codice di conferma.",
+    'otp_otpcode' => "Codice OTP",
+    'otp_submit' => "Sottoscrivi",
+    'otp_resend_otp' => "Reinvia OTP",
+
+    // Signup success
+    'signup_success_title' => 'Account creato',
+    'signup_success_message' => "Il tuo account è stato creato con successo. È in corso di revisione. È possibile accedere in account al momento dell'approvazione dell'account.",
+    'signup_success_button' => "Qui puoi accedere al tuo account",
+
+    'rating_page_title' => 'Valutazione e recensioni',
+    'rating_product_title' => 'Prodotto Adblue',
+    'rating_star_top' => 'Valuta questo prodotto',
+    'rating_right_review' => 'Condividi la tua recensione per il prodotto.',
+    'rating_title' => 'Titolo',
+    'rating_review' => 'Il tuo commento',
+    'rating_submit' => 'Invia recensione',
+
+];
